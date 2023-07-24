dbe37137b2277558e722305563dfbe08d43095ef
AliHussainWaris <mohammedalihussainwaris@gmail.com>
2023-07-24 14:25:41 +0500

    Tenth

--- src/components/CarMenu.js
+++ src/components/CarMenu.js
@@ -8,25 +8,28 @@ const CarMenu = (props) => {
         <>
             <section className={style.main}>
                 <div className={style.imgSection}>
-                    <img src={props.image} />
+                    <img src={props.image} alt="Car_Menu"/>
                 </div>
                 <div className={style.details}>
                     <h1>{props.name}</h1>
                     <hr />
                     <Link to={props.name}>
-                        <a href="#"><p>Configure</p></a>
+                        <p>Configure</p>
                     </Link>
 
                     <hr />
-                    <a href="#"><p>Comapre</p></a>
+                    <Link to="#"><p>Comapre</p></Link>
                     <hr />
-                    <a href="#"><p>Find New and Pre-owned Vehicles</p></a>
+                    <Link to="#"><p>Find New and Pre-owned Vehicles</p></Link>
 
                 </div>
                 <div className={style.models}>
                     <Link to={props.name}>
                         <div className={style.box}>
-                            <p><img src="https://cdn.ui.porsche.com/porsche-design-system/marque/porsche-marque-trademark.medium.min.fff6e9b91481cc5b1fc6c9b62987ccaf@2x.webp" width="20px" height="20px" />{props.name} Models</p>
+                            <p><img 
+                                src="https://cdn.ui.porsche.com/porsche-design-system/marque/porsche-marque-trademark.medium.min.fff6e9b91481cc5b1fc6c9b62987ccaf@2x.webp" width="20px" height="20px" 
+                                alt="model_image"
+                                />{props.name} Models</p>
                         </div>
                     </Link>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import CarMenu from './CarMenu'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { Link } from 'react-router-dom'
 import MediaQuery from 'react-responsive'
-import { Bag, GeoAlt, Person, ThreeDotsVertical } from 'react-bootstrap-icons'
+import { Bag, GeoAlt, Person } from 'react-bootstrap-icons'
 
 
 const Navbar = () => {
   const [Hover, setHover] = useState(false);
   let [num, setNum] = useState(0);
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
 
   const mouseOver = () => {
@@ -40,51 +40,51 @@ const Navbar = () => {
   let name = "911";
   let imagesrc;
   function check() {
-    if (num == 1) {
+    if (num === 1) {
       name = "718";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/718@2x.jpg";
     }
-    else if (num == 2) {
+    else if (num === 2) {
       name = "911";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/911@2x.jpg";
     }
-    else if (num == 3) {
+    else if (num === 3) {
       name = "Taycan";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/taycan@2x.jpg";
     }
-    else if (num == 4) {
+    else if (num === 4) {
       name = "Panamera";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/panamera@2x.jpg";
     }
-    else if (num == 5) {
+    else if (num === 5) {
       name = "Macan";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/macan@2x.jpg";
     }
-    else if (num == 6) {
+    else if (num === 6) {
       name = "Cayenne";
       imagesrc = "https://nav.porsche.com/00BC524/series-assets/1024/cayenne@2x.jpg";
     }
 
 
   }
-  const handleShow = () => {
-    setShow(true);
-    console.log('from handle show')
-  }
-  const handleNotShow = () => {
-    setShow(false);
-    console.log('from not handle show')
-  }
-  function check2() {
-    if (show == true) {
+  // const handleShow = () => {
+  //   setShow(true);
+  //   console.log('from handle show')
+  // }
+  // const handleNotShow = () => {
+  //   setShow(false);
+  //   console.log('from not handle show')
+  // }
+  // function check2() {
+  //   if (show === true) {
 
-      console.log("check2 is true")
-    } else if (show == false) {
-      console.log("check2 is false");
-      <CarMenu name={name} image={imagesrc} />
-    }
+  //     console.log("check2 is true")
+  //   } else if (show === false) {
+  //     console.log("check2 is false");
+  //     <CarMenu name={name} image={imagesrc} />
+  //   }
 
-  }
+  // }
   function showMenuFunc() {
     showMenu ? setShowMenu(false) : setShowMenu(true)
   }
@@ -94,32 +94,32 @@ const Navbar = () => {
      <MediaQuery minWidth={1216}>
       <div className={style.navbar} >
         <Link to={'/'}>
-          <img src="https://cdn.ui.porsche.com/porsche-design-system/marque/porsche-marque-trademark.medium.min.fff6e9b91481cc5b1fc6c9b62987ccaf@2x.webp" className={style.logo} />
+          <img src="https://cdn.ui.porsche.com/porsche-design-system/marque/porsche-marque-trademark.medium.min.fff6e9b91481cc5b1fc6c9b62987ccaf@2x.webp" className={style.logo} alt="Logo"/>
         </Link>
 
         <div className={style.modelImager} > 
           <div id="one" onMouseOverCapture={() => { mouseOver(); setNumber1() }} onMouseLeave={mouseLeave} className={style.model} >
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/718@2x.jpg" width="100px" />
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/718@2x.jpg" width="100px" alt="718"/>
             <p>718</p>
           </div>
           <div id="two" onMouseOverCapture={() => { mouseOver(); setNumber2() }} onMouseLeave={mouseLeave} className={style.model}>
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/911@2x.jpg" width="100px"/>
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/911@2x.jpg" width="100px" alt="911"/>
             <p>911</p>
           </div>
           <div onMouseOver={() => { mouseOver(); setNumber3() }} onMouseLeave={mouseLeave} className={style.model}>
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/taycan@2x.jpg" width="100px"/>
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/taycan@2x.jpg" width="100px" alt='Taycan'/>
             <p>Taycan</p>
           </div>
           <div onMouseOver={() => { mouseOver(); setNumber4() }} onMouseLeave={mouseLeave} className={style.model}>
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/panamera@2x.jpg" width="100px"/>
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/panamera@2x.jpg" width="100px" alt='Panamera'/>
             <p>Panamera</p>
           </div>
           <div onMouseOver={() => { mouseOver(); setNumber5() }} onMouseLeave={mouseLeave} className={style.model}>
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/macan@2x.jpg" width="100px"/>
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/macan@2x.jpg" width="100px" alt="Macan"/>
             <p>Macan</p>
           </div>
           <div onMouseOver={() => { mouseOver(); setNumber6() }} onMouseLeave={mouseLeave} className={style.model}>
-            <img src="https://nav.porsche.com/00BC524/series-assets/1024/cayenne@2x.jpg" width="100px"/>
+            <img src="https://nav.porsche.com/00BC524/series-assets/1024/cayenne@2x.jpg" width="100px" alt="Cayenne"/>
             <p>Cayenne</p>
           </div>
         </div>
@@ -137,18 +137,15 @@ const Navbar = () => {
               Dealers
             </li>
             <li>
-            <Link to="/Account">
+            <Link to="/Login">
              <Person/>
               My Porsche
              </Link>
              </li>
-            {/* <li>
-              <ThreeDotsVertical/>
-            </li> */}
           </ul>
         </div>
         <div className={style.btnContain} onClick={showMenuFunc}>
-          <img src="https://i.ibb.co/B6HwVZd/menu.png" className={style.menuBtn} />
+          <img src="https://i.ibb.co/B6HwVZd/menu.png" className={style.menuBtn} alt="Menu" />
         </div>
       </div>
       </MediaQuery>
@@ -157,48 +154,48 @@ const Navbar = () => {
      <MediaQuery maxWidth={1215}>
       <div className={style.navbar2} style={{height: !showMenu ? "50px" : "120vh", position: showMenu ? "relative" : "static" }}>
         <Link to={'/'}>
-          <img src="https://i.ibb.co/wBWTfPw/Porsche-logo.png" className={style.logo} />
+          <img src="https://i.ibb.co/wBWTfPw/Porsche-logo.png" alt="Logo" className={style.logo} />
         </Link>
 
         
         
           <div className={style.modelImages} style={{ display: !showMenu ? "none" : "flex", flexDirection: "row" }}>
             <div id="one" onMouseOverCapture={() => { mouseOver(); setNumber1() }} onMouseLeave={mouseLeave} className={style.model} > 
-              <img src="https://i.ibb.co/W2m2cpR/718.jpg" />
+              <img src="https://i.ibb.co/W2m2cpR/718.jpg" alt="718" />
               <p>718</p>
             </div>
             <div id="two" onMouseOverCapture={() => { mouseOver(); setNumber2() }} onMouseLeave={mouseLeave} className={style.model}>
-              <img src="https://i.ibb.co/B4dyXG7/911.jpg" />
+              <img src="https://i.ibb.co/B4dyXG7/911.jpg" alt="911"/>
               <p>911</p>
             </div>
             <div onMouseOver={() => { mouseOver(); setNumber3() }} onMouseLeave={mouseLeave} className={style.model}>
-              <img src="https://i.ibb.co/25FzmxQ/taycan.jpg" />
+              <img src="https://i.ibb.co/25FzmxQ/taycan.jpg" alt="Taycan"/>
               <p>Taycan</p>
             </div>
             <div onMouseOver={() => { mouseOver(); setNumber4() }} onMouseLeave={mouseLeave} className={style.model}>
-              <img src="https://i.ibb.co/JrFNvpf/panamera.jpg" />
+              <img src="https://i.ibb.co/JrFNvpf/panamera.jpg" alt="Panamera"/>
               <p>Panamera</p>
             </div>
             <div onMouseOver={() => { mouseOver(); setNumber5() }} onMouseLeave={mouseLeave} className={style.model}>
-              <img src="https://i.ibb.co/tqSVJCR/macan.jpg" />
+              <img src="https://i.ibb.co/tqSVJCR/macan.jpg" alt="Macan"/>
               <p>Macan</p>
             </div>
             <div onMouseOver={() => { mouseOver(); setNumber6() }} onMouseLeave={mouseLeave} className={style.model}>
-              <img src="https://i.ibb.co/6swS1s3/cayenne.jpg" />
+              <img src="https://i.ibb.co/6swS1s3/cayenne.jpg" alt="Cayenee"/>
               <p>Cayenne</p>
             </div>
           </div>
         
         <div className={style.links} >
           <ul>
-            <li><img src="https://i.ibb.co/RvQCXbk/shopping-bag.png" width="20px" height="20px" />Shop</li>
-            <li><img src="https://i.ibb.co/cc8LV03/location.png" width="20px" height="20px" />Dealers</li>
-            <li><img src="https://i.ibb.co/K0vQd1G/person.png" width="20px" height="20px" />My Porsche</li>
+            <li><img src="https://i.ibb.co/RvQCXbk/shopping-bag.png" width="20px" height="20px" alt="Shop" />Shop</li>
+            <li><img src="https://i.ibb.co/cc8LV03/location.png" width="20px" height="20px" alt="Dealers"/>Dealers</li>
+            <li><img src="https://i.ibb.co/K0vQd1G/person.png" width="20px" height="20px" alt="My Porsche"/>My Porsche</li>
             {/* <li><img src="https://i.ibb.co/mDYgtqb/dots.png" width="20px" height="20px" className={style.dots} /></li> */}
           </ul>
         </div>
         <div className={style.btnContain} onClick={showMenuFunc}>
-          <img src="https://i.ibb.co/B6HwVZd/menu.png" className={style.menuBtn} />
+          <img src="https://i.ibb.co/B6HwVZd/menu.png" alt="Menu" className={style.menuBtn} />
         </div>
       </div>
       </MediaQuery>
